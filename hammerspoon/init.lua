281545ab7c13ad698ea41a7734e452286f70399e
Gabe Conradi <gabe.conradi@datadoghq.com>
2021-01-31 09:55:01 -0600

    adding hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,4 @@
+--[[
 hs.window.animationDuration = 0
 units = {
   right30       = { x = 0.70, y = 0.00, w = 0.30, h = 1.00 },
@@ -23,3 +24,40 @@ hs.hotkey.bind(mash, '[', function() hs.window.focusedWindow():move(units.upleft
 hs.hotkey.bind(mash, ';', function() hs.window.focusedWindow():move(units.botleft70,  nil, true) end)
 hs.hotkey.bind(mash, "'", function() hs.window.focusedWindow():move(units.botright30, nil, true) end)
 hs.hotkey.bind(mash, 'm', function() hs.window.focusedWindow():move(units.maximum,    nil, true) end)
+--]]
+--
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  hs.alert.show("Hello World!")
+end)
+
+-- simple window movements
+hs.loadSpoon("WindowHalfsAndThirds")
+-- TODO(gabe): dont use animation durations when iterm is the window, cause its reflow
+-- is mad slow
+-- hs.window.animationDuration = 0.05
+hs.window.animationDuration = 0 -- aint nobody got time for this
+local hyper = {"shift", "cmd"}
+win = spoon.WindowHalfsAndThirds:bindHotkeys({
+  left_half   = { hyper, "Left" },
+  right_half  = { hyper, "Right" },
+  --top_half    = { hyper, "Up" },
+  --bottom_half = { hyper, "Down" },
+  --third_left  = { hyper, "Left" },
+  --third_right = { hyper, "Right" },
+  --third_up    = { hyper, "Up" },
+  --third_down  = { hyper, "Down" },
+  --top_left    = { hyper, "1" },
+  --top_right   = { hyper, "2" },
+  --bottom_left = { hyper, "3" },
+  --bottom_right= { hyper, "4" },
+  max_toggle  = { hyper, "f" },
+  max         = { hyper, "Up" },
+  --undo        = { hyper, "z" },
+  --center      = { hyper, "c" },
+  --larger      = { hyper, "Right" },
+  --smaller     = { hyper, "Left" },
+})
+
+-- https://www.hammerspoon.org/go/#pasteblock
+
+hs.hotkey.bind({"cmd", "alt"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
