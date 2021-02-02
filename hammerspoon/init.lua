d159d4505f51e0fa977abfa95d6e688a1ed5772a
Gabe Conradi <gabe.conradi@datadoghq.com>
2021-02-02 13:14:25 -0600

    hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,42 +1,10 @@
---[[
-hs.window.animationDuration = 0
-units = {
-  right30       = { x = 0.70, y = 0.00, w = 0.30, h = 1.00 },
-  right70       = { x = 0.30, y = 0.00, w = 0.70, h = 1.00 },
-  left70        = { x = 0.00, y = 0.00, w = 0.70, h = 1.00 },
-  left30        = { x = 0.00, y = 0.00, w = 0.30, h = 1.00 },
-  top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
-  bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
-  upright30     = { x = 0.70, y = 0.00, w = 0.30, h = 0.50 },
-  botright30    = { x = 0.70, y = 0.50, w = 0.30, h = 0.50 },
-  upleft70      = { x = 0.00, y = 0.00, w = 0.70, h = 0.50 },
-  botleft70     = { x = 0.00, y = 0.50, w = 0.70, h = 0.50 },
-  maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 }
-}
-
-mash = { 'shift', 'ctrl', 'cmd' }
-hs.hotkey.bind(mash, 'l', function() hs.window.focusedWindow():move(units.right30,    nil, true) end)
-hs.hotkey.bind(mash, 'h', function() hs.window.focusedWindow():move(units.left70,     nil, true) end)
-hs.hotkey.bind(mash, 'k', function() hs.window.focusedWindow():move(units.top50,      nil, true) end)
-hs.hotkey.bind(mash, 'j', function() hs.window.focusedWindow():move(units.bot50,      nil, true) end)
-hs.hotkey.bind(mash, ']', function() hs.window.focusedWindow():move(units.upright30,  nil, true) end)
-hs.hotkey.bind(mash, '[', function() hs.window.focusedWindow():move(units.upleft70,   nil, true) end)
-hs.hotkey.bind(mash, ';', function() hs.window.focusedWindow():move(units.botleft70,  nil, true) end)
-hs.hotkey.bind(mash, "'", function() hs.window.focusedWindow():move(units.botright30, nil, true) end)
-hs.hotkey.bind(mash, 'm', function() hs.window.focusedWindow():move(units.maximum,    nil, true) end)
---]]
---
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.alert.show("Hello World!")
-end)
-
 -- simple window movements
 hs.loadSpoon("WindowHalfsAndThirds")
 -- TODO(gabe): dont use animation durations when iterm is the window, cause its reflow
 -- is mad slow
 -- hs.window.animationDuration = 0.05
 hs.window.animationDuration = 0 -- aint nobody got time for this
-local hyper = {"shift", "cmd"}
+local hyper = {"cmd"}
 win = spoon.WindowHalfsAndThirds:bindHotkeys({
   left_half   = { hyper, "Left" },
   right_half  = { hyper, "Right" },
@@ -50,8 +18,8 @@ win = spoon.WindowHalfsAndThirds:bindHotkeys({
   --top_right   = { hyper, "2" },
   --bottom_left = { hyper, "3" },
   --bottom_right= { hyper, "4" },
-  max_toggle  = { hyper, "f" },
-  max         = { hyper, "Up" },
+  --max_toggle  = { hyper, "f" },
+  max_toggle  = { hyper, "Up" },
   --undo        = { hyper, "z" },
   --center      = { hyper, "c" },
   --larger      = { hyper, "Right" },
@@ -61,3 +29,16 @@ win = spoon.WindowHalfsAndThirds:bindHotkeys({
 -- https://www.hammerspoon.org/go/#pasteblock
 
 hs.hotkey.bind({"cmd", "alt"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+
+--[[
+-- If AppGate is running, try to figure out which connection we are on
+function applicationWatcher(appName, eventType, appObject)
+    if (eventType == hs.application.watcher.activated) then
+        if (appName == "AppGate SDP") then
+          hs.alert.show("AppGate Focused" .. appName .. "--" )
+        end
+    end
+end
+appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
+--]]
