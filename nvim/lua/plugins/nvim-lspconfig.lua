bc1999c8f0239b03b9d1ce27c2c5d095d9d40c93
Gabe Conradi <gconradi@seatgeek.com>
2026-02-19 14:22:37 -0800

    nvim plugins

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,5 +4,6 @@ return {
   config = function()
     vim.lsp.enable("gopls")        -- requires `gopls` installed
     vim.lsp.enable("terraformls") -- requires https://github.com/hashicorp/terraform-ls/blob/main/docs/installation.md
+    vim.lsp.enable("pylsp") --
   end
 }
