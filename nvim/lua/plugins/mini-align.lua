01585cd81d50246a55608718d9c15a8597cd66af
Gabe Conradi <gconradi@seatgeek.com>
2026-06-14 17:54:36 -0700

    mini-align nvim plugin

---
+++ nvim/lua/plugins/mini-align.lua
@@ -0,0 +1,7 @@
+-- Visual select lines, then ga<char> to align on <char> (e.g. ga= for =).
+-- gA<char> for live preview mode.
+return {
+  'echasnovski/mini.align',
+  version = '*',
+  opts = {},
+}
