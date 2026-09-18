fe2aba41ebf68a21fefd11c4aac0cc8eff2e2de9
Gabe Conradi <gconradi@seatgeek.com>
2026-09-18 09:08:28 -0500

    move LSP keymaps into LspAttach callback so they only work when LSP is running

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -20,42 +20,46 @@ vim.keymap.set("n", "<C-p>", ":Ag<CR>", opts)
 -- fzf: ctrl-l for fuzzy search across all open buffer lines
 vim.keymap.set("n", "<C-l>", ":Lines<CR>", opts)
 
--- LSP: go to declaration (rarely used, most servers support definition instead)
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-
--- LSP: go to definition (jump to where the symbol is defined)
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-
--- LSP: hover docs (show type info / documentation for symbol under cursor)
-vim.keymap.set("n", "<C-h>", vim.lsp.buf.hover, opts)
-
--- LSP: go to implementation (useful for interfaces)
-vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-
--- LSP: signature help (show function parameter info)
--- conflicts with blink-cmp's ctrl-k in insert mode, but this is normal mode only
-vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-
--- LSP: go to type definition (jump to the type of the symbol under cursor)
-vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-
--- LSP: rename symbol across the project
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-
--- LSP: find all references to the symbol under cursor
-vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-
--- diagnostics: jump to next/previous error or warning
-vim.keymap.set("n", "]g", vim.diagnostic.goto_next, opts)
-vim.keymap.set("n", "[g", vim.diagnostic.goto_prev, opts)
-
--- format on save: auto-format buffer using LSP when writing
+-- LSP keymaps and format-on-save: only active in buffers with an LSP server
 -- https://www.mitchellhanberg.com/modern-format-on-save-in-neovim/
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("lsp", { clear = true }),
   callback = function(args)
+    local buf = args.buf
+    local bufopts = { noremap = true, silent = true, buffer = buf }
+
+    -- go to declaration (rarely used, most servers support definition instead)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+
+    -- go to definition (jump to where the symbol is defined)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+
+    -- hover docs (show type info / documentation for symbol under cursor)
+    vim.keymap.set("n", "<C-h>", vim.lsp.buf.hover, bufopts)
+
+    -- go to implementation (useful for interfaces)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+
+    -- signature help (show function parameter info)
+    -- conflicts with blink-cmp's ctrl-k in insert mode, but this is normal mode only
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+
+    -- go to type definition (jump to the type of the symbol under cursor)
+    vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+
+    -- rename symbol across the project
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+
+    -- find all references to the symbol under cursor
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+
+    -- diagnostics: jump to next/previous error or warning
+    vim.keymap.set("n", "]g", vim.diagnostic.goto_next, bufopts)
+    vim.keymap.set("n", "[g", vim.diagnostic.goto_prev, bufopts)
+
+    -- format on save: auto-format buffer using the attaching LSP server
     vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = args.buf,
+      buffer = buf,
       callback = function()
         vim.lsp.buf.format({ async = false, id = args.data.client_id })
       end,
